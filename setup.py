9951a52fd5d78d6b85602e826cb3120840723be3
muyiwao <muyiwa.oladimeji@yahoo.com>
2023-02-22 11:29:00 +0000

    update files

--- setup.py
+++ setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup
 
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
 setup(
     name="EnergyTrendsDataDownloader",
-    version="1.0.1",
+    version="1.0.0",
     description="A demo package to automate the downloading and cleaning trend data from the UK Government's website",
-    long_description=open("README.md").read(),
-    long_description_content_type="text/markdown",
+    long_description=readme(),
+    long_description_content_type="text/x-rst",
     url="https://github.com/muyiwao/PetroineosSolution",
-    author="Muyiwa O",
-    author_email="muyirays@gmail.com",
-    license="MIT",
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-    ],
-    packages=["EnergyTrendsDataDownloader"],
-    include_package_data=True,
-    python_requires=">=3.0",
+    author="muyiwa",
+    author_email="muyirays@gmail.com"
 )
